ded22008f2b4f79bdfbb331288409afe49a9b2c9
Adam <sromovskyadam@gmail.com>
2024-02-22 23:51:21 +0100

    Added gluestack-ui library

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FoodCreateScreen from './src/screens/food-create-screen';
 import FoodStorageScreen from './src/screens/food-storage-screen';
 import FoodStatsScreen from './src/screens/food-stats';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import { useColorScheme } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,16 +16,20 @@ const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL, {
 });
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
   return (
     <ConvexProvider client={convex}>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <Tab.Navigator >
-          <Tab.Screen name="Storage" component={FoodStorageScreen} options={{title: 'Available food'}} />
-          <Tab.Screen name="FoodCreate" component={FoodCreateScreen} options={{tabBarItemStyle: {display: 'none'}, title: 'Create New Food'}} />
-          <Tab.Screen name="Stats" component={FoodStatsScreen} options={{title: 'Stats'}} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <GluestackUIProvider config={config} colorMode={colorScheme}>
+        <NavigationContainer theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <StatusBar style="auto" />
+          <Tab.Navigator >
+            <Tab.Screen name="Storage" component={FoodStorageScreen} options={{title: 'Available food'}} />
+            <Tab.Screen name="FoodCreate" component={FoodCreateScreen} options={{tabBarItemStyle: {display: 'none'}, title: 'Create New Food'}} />
+            <Tab.Screen name="Stats" component={FoodStatsScreen} options={{title: 'Stats'}} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </GluestackUIProvider>
     </ConvexProvider>
   );
 }
